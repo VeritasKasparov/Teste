5c6a6108a0a06ead0909c02e874cbf4652360f64
VeritasKasparov <stanford42@protonmail.com>
2022-03-20 23:34:44 -0300

    hello

---
+++ .teste/index2.c
@@ -0,0 +1,6 @@
+int main(){
+	int *m;
+	m = (int*) 0x7fff9a8be434;
+	(*m) = 0;
+	return 0;	
+}
---
+++ index.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <sys/socket.h>
+
+int teste(){
+    return 0;
+}
+void main(int teste()){
+    printf("hello world %d\n", &teste);};
